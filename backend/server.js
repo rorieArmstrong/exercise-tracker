74ce410aa37a55b0471cc462fd1d52bd1aab2bcf
rorieArmstrong <61753930+rorieArmstrong@users.noreply.github.com>
2020-06-22 16:39:34 +0100

    update git ignore to include database

--- backend/server.js
+++ backend/server.js
@@ -21,11 +21,6 @@ const db = require("./app/models");
 const Role = db.role;
 
 db.sequelize.sync();
-// force: true will drop the table if it already exists
-// db.sequelize.sync({force: true}).then(() => {
-//   console.log('Drop and Resync Database with { force: true }');
-//   initial();
-// });
 
 // simple route
 app.get("/", (req, res) => {
