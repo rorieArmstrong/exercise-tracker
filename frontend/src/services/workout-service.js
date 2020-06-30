1beba158c6f3b279cc89a67f2a323af874d373fe
rorieArmstrong <61753930+rorieArmstrong@users.noreply.github.com>
2020-06-30 10:35:30 +0100

    changed post to put

--- frontend/src/services/workout-service.js
+++ frontend/src/services/workout-service.js
@@ -5,19 +5,19 @@ const API_URL = 'http://localhost:8080/api/workouts/';
 
 class UserService {
   getPublicContent() {
-    return axios.get(API_URL + 'all');
+    return axios.get(API_URL + 'getall');
   }
 
   getUserBoard() {
-    return axios.get(API_URL + 'user', { headers: authHeader() });
+    return axios.get(API_URL + 'getone', { headers: authHeader() });
   }
 
-  getModeratorBoard() {
-    return axios.get(API_URL + 'mod', { headers: authHeader() });
+  getModeratorBoard(data) {
+    return axios.post(API_URL + 'create', data , { headers: authHeader() });
   }
 
   getAdminBoard() {
-    return axios.get(API_URL + 'admin', { headers: authHeader() });
+    return axios.put(API_URL + 'update', { headers: authHeader() });
   }
 }
 
