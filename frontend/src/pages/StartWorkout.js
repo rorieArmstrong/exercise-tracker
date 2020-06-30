1288b88259af2921cc5306b6dd6a8b94812348bd
rorieArmstrong <61753930+rorieArmstrong@users.noreply.github.com>
2020-06-30 13:59:28 +0100

    update details

--- frontend/src/pages/StartWorkout.js
+++ frontend/src/pages/StartWorkout.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import AuthService from "../services/auth.service";
+import Workout from '../components/Workout'
 
 class StartWorkout extends Component {
     render() {
