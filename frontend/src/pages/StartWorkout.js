bf0f154e1131ec68ac579e93668b01c526326947
rorieArmstrong <61753930+rorieArmstrong@users.noreply.github.com>
2020-07-15 10:30:04 +0100

    add services

--- frontend/src/pages/StartWorkout.js
+++ frontend/src/pages/StartWorkout.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import AuthService from "../services/auth.service";
 import Workout from '../components/Workout'
+import WorkoutService from '../services/workout-service';
 
 class StartWorkout extends Component {
     render() {
